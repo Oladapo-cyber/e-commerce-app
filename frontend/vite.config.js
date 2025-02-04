e5b4b6a78237a289f04c1df521725cfa9e1be25d
daps <ydapo50@gmail.com>
2025-02-04 13:41:20 +0100

    integrate Tailwind CSS and remove unused styles from App.css and index.css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// Vite configuration file for setting up plugins and other options
+// For more details, visit: https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+});
