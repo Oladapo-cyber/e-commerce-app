9c90408478ca2e03dfd6704311d8550d7ff8681b
daps <ydapo50@gmail.com>
2025-02-09 01:50:47 +0100

    add ShopListing page, update Navbar with search and authentication buttons, and include routing for new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,14 @@ import styled, { ThemeProvider } from "styled-components";
 import { lightTheme } from "./utils/Themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import NewArrivals from "./pages/NewArrivals";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import ShopListing from "./pages/ShopListing";
+import Authentication from "./pages/Authentication";
 const Container = styled.div`
   background: ${(props) => props.theme.bg};
   color: ${(props) => props.theme.text_primary};
@@ -19,6 +27,20 @@ const App = () => {
     <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/newarrivals" element={<NewArrivals />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/shoplisting" element={<ShopListing />} />
+          <Route path="*" element={<NotFound />} />
+          {/* <Route path="/checkout" element={<Checkout />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { Login } from "@mui/icons-material";
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <div className="bg-navbar flex items-center justify-between py-4 px-6 shadow-sm fixed w-full z-30">
+      <h1 className="text-3xl font-cursive">Dapstore</h1>
+      <div className="flex items-center space-x-6">
+        <input
+          type="text"
+          placeholder="Search for products..."
+          className="w-64 px-3 py-2 bg-gray-200 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all"
+        />
+        <ul className="flex items-center space-x-4 text-lg">
+          <li>Categories</li>
+          <li>Deals</li>
+          <li>New</li>
+          <li>Delivery</li>
+        </ul>
+        <div className="flex items-center space-x-4">
+          <button className="bg-gray-200 text-black px-4 py-2 rounded-lg">
+            <Login />
+            Login
+          </button>
+          <button className="bg-gray-200 text-black px-4 py-2 rounded-lg">
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
