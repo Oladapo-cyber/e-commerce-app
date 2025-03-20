d0be89f77a9fa43eed97c02b20838c1153e82ed2
daps <ydapo50@gmail.com>
2025-03-20 17:35:55 +0100

    add backend server setup with Express and MongoDB connection

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+/* global process */
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGO_DB)
+    .then(() => console.log("Connected to mongo DB successfully"))
+    .catch((err) => {
+      console.error("Failed to connect with mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
