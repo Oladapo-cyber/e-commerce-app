b6d8e8993bef4986ef3fb1163a3ef6af460b204a
daps <ydapo50@gmail.com>
2025-03-26 10:00:46 +0100

    enhance server setup by adding product routes and improving error handling middleware

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,27 @@
 /* global process */
+
+// Import necessary modules and dependencies
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import UserRouter from "./routes/User.js";
+import ProductRoutes from "./routes/Products.js";
 
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
 
-//Error handling
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong!";
+// Middleware setup
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(express.json({ limit: "50mb" })); // Parse JSON requests with a size limit
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+
+// Error-handling middleware
+app.use((err, req, res) => {
+  const status = err.status || 500; // Default to status 500 if not provided
+  const message = err.message || "Something went wrong!"; // Default error message
   return res.status(status).json({
     success: false,
     status,
@@ -23,17 +29,22 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Define a basic route for testing the server
 app.get("/", async (req, res) => {
   res.status(200).json({
-    message: "Hello World",
+    message: "Hello World", // Response message
   });
 });
-app.use("/api/user", UserRouter);
 
+// API routes
+app.use("/api/user", UserRouter); // User-related routes
+app.use("/api/products", ProductRoutes); // Product-related routes
+
+// Function to connect to MongoDB
 const connectDB = () => {
-  mongoose.set("strictQuery", true);
+  mongoose.set("strictQuery", true); // Enable strict query mode
   mongoose
-    .connect(process.env.MONGO_DB)
+    .connect(process.env.MONGO_DB) // Connect to MongoDB using the connection string from .env
     .then(() => console.log("Connected to mongo DB successfully"))
     .catch((err) => {
       console.error("Failed to connect with mongo");
@@ -41,13 +52,15 @@ const connectDB = () => {
     });
 };
 
+// Function to start the server
 const startServer = async () => {
   try {
-    connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    connectDB(); // Establish database connection
+    app.listen(8080, () => console.log("Server started on port 8080")); // Start the server on port 8080
   } catch (error) {
-    console.log(error);
+    console.log(error); // Log any errors during server startup
   }
 };
 
+// Start the server
 startServer();
