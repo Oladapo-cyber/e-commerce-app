89aa9830a55435dbc4b3e8fdcda0e8acaa225e78
daps <ydapo50@gmail.com>
2025-04-20 04:08:00 +0100

    feat: implement loading state and product fetching in ShopListing; add CircularProgress for loading indication and update product display logic

--- frontend/src/pages/ShopListing.jsx
+++ frontend/src/pages/ShopListing.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
-import { Slider } from "@mui/material";
+import { CircularProgress, Slider } from "@mui/material";
 import ProductCard from "../components/cards/ProductCard";
 import CardWrapper from "../components/CardWrapper";
 import { filter } from "../utils/data.js";
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { getAllProducts } from "../api/index.js";
 
 const Filters = ({ children }) => {
   return (
@@ -39,79 +40,111 @@ const SelectableItem = ({ children, selected, onClick }) => {
 };
 
 const ShopListing = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
   const [priceRange, setPriceRange] = useState([0, 1000]);
-  const [selectedSizes, setSelectedSizes] = useState([""]);
-  const [selectedCategories, setSelectedCategories] = useState([""]);
+  const [selectedSizes, setSelectedSizes] = useState(["S", "M", "L", "XL"]);
+  const [selectedCategories, setSelectedCategories] = useState([
+    "Men",
+    "Women",
+    "Kids",
+    "Bags",
+  ]);
 
+  const getFilteredProducts = useCallback(async () => {
+    setLoading(true);
+
+    const res = await getAllProducts(
+      `minPrice=${priceRange[0]}&maxPrice=${priceRange[1]}${
+        selectedSizes.length > 0 ? `&sizes=${selectedSizes.join(",")}` : ""
+      }${
+        selectedCategories.length > 0
+          ? `&categories=${selectedCategories.join(",")}`
+          : ""
+      }`
+    );
+    setProducts(res.data);
+    setLoading(false);
+  }, [priceRange, selectedSizes, selectedCategories]);
+
+  useEffect(() => {
+    getFilteredProducts();
+  }, [getFilteredProducts]);
   return (
     <div className="flex flex-col sm:flex-row items-start sm:space-x-4 p-5 mx-auto">
-      <Filters>
-        <Menu>
-          {filter.map((filters) => (
-            <FilterSection key={filters.name}>
-              <h1 className="text-lg font-bold text-gray-900">
-                {filters.name}
-              </h1>
-              {filters.value === "price" ? (
-                <Slider
-                  defaultValue={priceRange}
-                  min={0}
-                  max={1000}
-                  valueLabelDisplay="auto"
-                  marks={[
-                    { value: 0, label: "$0" },
-                    { value: 1000, label: "$1000" },
-                  ]}
-                  onChange={(e, newValue) => setPriceRange(newValue)}
-                />
-              ) : filters.value === "size" ? (
-                <Item>
-                  {filters.items.map((item) => (
-                    <SelectableItem
-                      key={item}
-                      selected={selectedSizes.includes(item)}
-                      onClick={() =>
-                        setSelectedSizes((prev) =>
-                          prev.includes(item)
-                            ? prev.filter((category) => category !== item)
-                            : [...prev, item]
-                        )
-                      }
-                    >
-                      {item}
-                    </SelectableItem>
-                  ))}
-                </Item>
-              ) : filters.value === "category" ? (
-                <Item>
-                  {filters.items.map((item) => (
-                    <SelectableItem
-                      key={item}
-                      selected={selectedCategories.includes(item)}
-                      onClick={() =>
-                        setSelectedCategories((prev) =>
-                          prev.includes(item)
-                            ? prev.filter((category) => category !== item)
-                            : [...prev, item]
-                        )
-                      }
-                    >
-                      {item}
-                    </SelectableItem>
-                  ))}
-                </Item>
-              ) : null}
-            </FilterSection>
-          ))}
-        </Menu>
-      </Filters>
-      <CardWrapper>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </CardWrapper>
+      {loading ? (
+        <div className="w-full flex justify-center items-center h-full">
+          <CircularProgress />
+        </div>
+      ) : (
+        <>
+          <Filters>
+            <Menu>
+              {filter.map((filters) => (
+                <FilterSection key={filters.name}>
+                  <h1 className="text-lg font-bold text-gray-900">
+                    {filters.name}
+                  </h1>
+                  {filters.value === "price" ? (
+                    <Slider
+                      defaultValue={priceRange}
+                      min={0}
+                      max={1000}
+                      valueLabelDisplay="auto"
+                      marks={[
+                        { value: 0, label: "$0" },
+                        { value: 1000, label: "$1000" },
+                      ]}
+                      onChange={(e, newValue) => setPriceRange(newValue)}
+                    />
+                  ) : filters.value === "size" ? (
+                    <Item>
+                      {filters.items.map((item) => (
+                        <SelectableItem
+                          key={item}
+                          selected={selectedSizes.includes(item)}
+                          onClick={() =>
+                            setSelectedSizes((prev) =>
+                              prev.includes(item)
+                                ? prev.filter((category) => category !== item)
+                                : [...prev, item]
+                            )
+                          }
+                        >
+                          {item}
+                        </SelectableItem>
+                      ))}
+                    </Item>
+                  ) : filters.value === "category" ? (
+                    <Item>
+                      {filters.items.map((item) => (
+                        <SelectableItem
+                          key={item}
+                          selected={selectedCategories.includes(item)}
+                          onClick={() =>
+                            setSelectedCategories((prev) =>
+                              prev.includes(item)
+                                ? prev.filter((category) => category !== item)
+                                : [...prev, item]
+                            )
+                          }
+                        >
+                          {item}
+                        </SelectableItem>
+                      ))}
+                    </Item>
+                  ) : null}
+                </FilterSection>
+              ))}
+            </Menu>
+          </Filters>
+          <CardWrapper>
+            {products?.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </CardWrapper>
+        </>
+      )}
     </div>
   );
 };
